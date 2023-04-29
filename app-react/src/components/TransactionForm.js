41b701792417d590176915533f9c760ed173c3b5
Preeti <preetijav@gmail.com>
2023-04-29 20:54:22 +0200

    Added form component

--- app-react/src/components/TransactionForm.js
+++ app-react/src/components/TransactionForm.js
@@ -1,7 +1,113 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import { TransactionsContext } from "../contexts/TransactionContext";
 
 const TransactionForm = () => {
-  return <div>TransactionForm</div>;
+  const [errors, setErrors] = useState({});
+  const [formData, setFormData] = useState({
+    account_id: "",
+    amount: "",
+  });
+
+  const { addTransaction, setCurrentAccount } = useContext(TransactionsContext);
+
+  const validateForm = () => {
+    let errors = {};
+    const uuidv4regex = new RegExp(
+      "^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[4][0-9a-fA-F]{3}-[8-9aAbB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$"
+    );
+    const intregex = new RegExp("[^[d.-/]+$");
+
+    if (!formData.account_id || !uuidv4regex.test(formData.account_id)) {
+      errors.account_id = "Account ID must be a valid UUID v4";
+    }
+
+    if (!formData.amount || !intregex.test(formData.amount)) {
+      errors.amount = "Amount must be a valid integer";
+    }
+
+    return errors;
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const errors = validateForm();
+
+    const transaction = {
+      account_id: formData.account_id,
+      amount: formData.amount,
+    };
+
+    setTimeout(() => {
+      window.location.reload(true);
+    }, 4000);
+
+    if (Object.keys(errors).length === 0) {
+      await addTransaction(transaction);
+      setCurrentAccount(transaction.account_id);
+      setErrors({});
+    } else {
+      setErrors(errors);
+    }
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
+  };
+
+  return (
+    <>
+      <h3>Add new transaction</h3>
+      <form onSubmit={handleSubmit}>
+        <div className="form-control mb-3">
+          <label htmlFor="amountId" className="form-label">
+            Account Id
+          </label>
+          <input
+            data-type="account-id"
+            className="form-control"
+            id="account_id"
+            name="account_id"
+            value={formData.account_id}
+            onChange={handleChange}
+          />
+        </div>
+        {errors.account_id && (
+          <div className="text-danger">{errors.account_id}</div>
+        )}
+        <div className="form-control mb-3">
+          <label htmlFor="Amount" className="form-label">
+            Amount
+          </label>
+          <input
+            className="form-control"
+            aria-describedby="amount"
+            type="text"
+            data-type="amount"
+            id="amount"
+            name="amount"
+            value={formData.amount ?? ""}
+            onChange={handleChange}
+          />
+        </div>
+        {errors.amount && <div className="text-danger">{errors.amount}</div>}
+
+  
+          <div className="text-center d-flex align-items-center justify-content-center">
+            <button
+              data-type="transaction-submit"
+              type="submit"
+              id="submit"
+              variant="primary"
+              className="btn btn-primary"
+            >
+              Submit
+            </button>
+          </div>
+  
+      </form>
+    </>
+  );
 };
 
 export default TransactionForm;
