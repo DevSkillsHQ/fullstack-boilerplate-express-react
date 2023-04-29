d6e8c6cc98acac765878e52d153361b482260b03
Preeti <preetijav@gmail.com>
2023-04-29 23:45:46 +0200

    Added frontend tests

--- cypress/integration/frontend.spec.js
+++ cypress/integration/frontend.spec.js
@@ -1,5 +1,37 @@
-describe('Frontend Test Spec', () => {
-  it('should visit index', () => {
-    cy.visit('/')
-  })
-})
+describe("Frontend Test Spec", () => {
+  it("should visit index", () => {
+    cy.visit("http://localhost:3000/");
+  });
+
+  it("should display the component with correct props", () => {
+    it("renders the component without errors", () => {
+      cy.get("h3").should("have.text", "Transaction History");
+      cy.get("ul").should("exist");
+    });
+
+    it("displays the list of transactions correctly", () => {
+      cy.intercept("/transactions", { fixture: "transactions.json" });
+      cy.get("li").should("have.length", 2);
+      cy.get("li:first-child").should(
+        "have.text",
+        "64c61cb8-0ffe-4f6d-ac30-4ea615fe07d9 5"
+      );
+    });
+
+    it("Doesn't have a account Id", () => {
+      cy.get('input[name="account_id"]').clear();
+      cy.get(".details > p").should("contain", "You need a account Id!!!");
+    });
+
+    it("Has a account Id", () => {
+      cy.get('input[name="account_id"]')
+        .clear()
+        .type("64c61cb8-0ffe-4f6d-ac30-4ea615fe07d9");
+      cy.get(".details").should(
+        "contain",
+        "64c61cb8-0ffe-4f6d-ac30-4ea615fe07d9"
+      );
+      cy.get("button").should("be.enabled");
+    });
+  });
+});
