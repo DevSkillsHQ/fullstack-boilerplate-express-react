e3738283efefde579f21ffac45766112994a61d8
Preeti <preetijav@gmail.com>
2023-04-29 20:53:01 +0200

    changed the port

--- app-express/src/server/index.js
+++ app-express/src/server/index.js
@@ -5,7 +5,7 @@ const app = require("./app");
 
 const server = http.createServer(app);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 
 server.listen(PORT, error => {
 
