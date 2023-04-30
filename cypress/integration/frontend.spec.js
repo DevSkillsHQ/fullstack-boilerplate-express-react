87cb5ebf381f622bd4437f2e9b08a197c9315d5e
Preeti <preetijav@gmail.com>
2023-04-30 12:13:21 +0200

    changes to form testing

--- cypress/integration/frontend.spec.js
+++ cypress/integration/frontend.spec.js
@@ -5,7 +5,7 @@ describe("Frontend Test Spec", () => {
 
   it("should display the component with correct props", () => {
     it("renders the component without errors", () => {
-      cy.get("h3").should("have.text", "Transaction History");
+      cy.get("h4").should("have.text", "Transaction History");
       cy.get("ul").should("exist");
     });
 
@@ -19,14 +19,18 @@ describe("Frontend Test Spec", () => {
     });
 
     it("Doesn't have a account Id", () => {
-      cy.get('input[name="account_id"]').clear();
+      cy.get('input[name="from_account_id"]').clear();
+      cy.get('input[name="to_account_id"]').clear();
       cy.get(".details > p").should("contain", "You need a account Id!!!");
     });
 
     it("Has a account Id", () => {
-      cy.get('input[name="account_id"]')
+      cy.get('input[name="from_account_id"]')
         .clear()
         .type("64c61cb8-0ffe-4f6d-ac30-4ea615fe07d9");
+        cy.get('input[name="to_account_id"]')
+        .clear()
+        .type("d841aa5e-ae7b-4b07-9276-b762c62a7d99");
       cy.get(".details").should(
         "contain",
         "64c61cb8-0ffe-4f6d-ac30-4ea615fe07d9"
